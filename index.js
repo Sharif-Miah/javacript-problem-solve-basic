28f43b87eabaec1dd99df4f20f10e370610a75b7
farhan sharif <farhansharif1410@gmail.com>
2022-07-31 18:53:58 -0230

    first problem sovle

---
+++ index.js
@@ -0,0 +1,20 @@
+// Problem 1: 
+// Total Amount 1000 taka 
+// Cost buy orance and apples 700 take 
+// Reture take is 300 taka 
+
+// solving problem :
+
+var totalAmount = 1000;
+
+var mangoPrice = 300;
+var orancePrice = 400;
+
+var costAmount = (mangoPrice + orancePrice)
+
+
+var returenMoney = (totalAmount - costAmount);
+
+console.log(returenMoney);
+
+// Problem 2: 

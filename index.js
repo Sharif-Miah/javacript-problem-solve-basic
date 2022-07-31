8e2d3e3d274809659ff2b5c0c86e15388e1c46ad
farhan sharif <farhansharif1410@gmail.com>
2022-07-31 19:15:41 -0230

    second problem solve

--- index.js
+++ index.js
@@ -17,4 +17,30 @@ var returenMoney = (totalAmount - costAmount);
 
 console.log(returenMoney);
 
+
 // Problem 2: 
+
+// Five subject number below :
+
+// Bangla = 75.25
+// Biology = 65
+// Mathematice = 80
+// chemistry = 35.45
+// physics = 99.50
+
+var bangla = 75.25
+var biology = 65
+var mathematice = 80
+var chemistry = 35.45
+var physics = 99.50
+
+var allNumberAdded = (bangla + biology + mathematice + chemistry + physics)
+
+// parseInt 
+var averageMarks = parseInt(allNumberAdded / 5)
+console.log(averageMarks);
+
+// parseFloat 
+var averageMarks = parseFloat(allNumberAdded / 5)
+console.log(averageMarks);
+
